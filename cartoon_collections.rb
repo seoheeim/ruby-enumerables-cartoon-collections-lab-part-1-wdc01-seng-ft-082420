3f4a701b71a887c23c24762f1b6f92a0ed263d0c
Seohee  Im <seoheeim@vt.edu>
2020-07-26 00:17:12 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,12 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  array.each{ |n| puts "Hello #{n}!"}
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_with_index{|element, i| puts "#{i+1} #{element}"}
 end
